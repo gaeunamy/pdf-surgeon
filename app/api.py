171392e9c414a885e3b79071b1f646252b123a51
Kim Gaeun <kge2015@gmail.com>
2026-06-04 13:08:45 +0900

    fix: make CORS settings more explicit

--- app/api.py
+++ app/api.py
@@ -9,9 +9,12 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://pdf-surgeon-xi.vercel.app"
+    ],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
 )
 
